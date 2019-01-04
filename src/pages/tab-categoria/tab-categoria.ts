71270d8b9002feb1add3388896ed32a1768c1489
Rodrigo <r_ataide@hotmail.com>
2019-01-04 15:22:25 -0300

    Pages
    Criando as pages.

--- src/pages/tab-categoria/tab-categoria.ts
+++ src/pages/tab-categoria/tab-categoria.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the LoginPage page.
+ * Generated class for the TabCategoriaPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,20 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: 'page-tab-categoria',
+  templateUrl: 'tab-categoria.html',
 })
-export class LoginPage {
+export class TabCategoriaPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
-
-  abrirCategoria(){
-    this.navCtrl.setRoot('CategoriaPage');
+    console.log('ionViewDidLoad TabCategoriaPage');
   }
 
 }
